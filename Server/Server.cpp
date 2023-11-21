7c92ba6ad8b92fb1c23adbf1e81d43963b0ea0ca
Alex-dev02 <ionut02alex@yahoo.ro>
2023-11-21 21:18:05 +0200

    Server implementation for accepting requests

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <thread>
+
 #include "Server.hpp"
+
+Server::Server(
+    std::unique_ptr<TcpListener> listener,
+    std::shared_ptr<std::queue<std::unique_ptr<NetworkStream>>> streamQueue,
+    std::shared_ptr<std::atomic_uint32_t> queuedStreams,
+    std::shared_ptr<std::mutex> mtx,
+    std::shared_ptr<std::condition_variable> cond
+)
+    : m_listener(std::move(listener)),
+    m_streamQueue(streamQueue),
+    m_queuedStreams(queuedStreams),
+    m_mtx(mtx),
+    m_cond(cond)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+    }
+
+void Server::Listen() {
+    m_listener->Start();
+    
+    std::cout << "Server successfully started listening\n";
+
+    while (true) {
+        std::unique_ptr<NetworkStream> stream = m_listener->AcceptClient();
+        m_mtx->lock();
+        (*m_queuedStreams)++;
+        m_streamQueue->push(std::move(stream));
+        std::cout << "New requested added to queue\n";
+        if ((*m_queuedStreams) == 1)
+            m_cond->notify_one();
+        m_mtx->unlock();
+    }
+}
--- Server/Server.hpp
+++ Server/Server.hpp
@@ -1,9 +1,34 @@
 #pragma once
 
+#include <memory>
+#include <atomic>
+#include <mutex>
+#include <queue>
+#include <condition_variable>
+#include <string>
+
+#include <Networking/TcpListener.hpp>
+#include <Networking/NetworkStream.hpp>
+
 class Server {
 
 public:
+    Server(
+        std::unique_ptr<TcpListener> listener,
+        std::shared_ptr<std::queue<std::unique_ptr<NetworkStream>>> streamQueue,
+        std::shared_ptr<std::atomic_uint32_t> queuedStreams,
+        std::shared_ptr<std::mutex> mtx,
+        std::shared_ptr<std::condition_variable> cond
+    );
+    Server(const Server&) = delete;
+    Server& operator= (const Server&) = delete;
+
+    void Listen();
 
 private:
-
+    std::unique_ptr<TcpListener> m_listener;
+    std::shared_ptr<std::queue<std::unique_ptr<NetworkStream>>> m_streamQueue;
+    std::shared_ptr<std::atomic_uint32_t> m_queuedStreams;
+    std::shared_ptr<std::mutex> m_mtx;
+    std::shared_ptr<std::condition_variable> m_cond;
 };
