6425ca8f6047cc330a3bdc49b897c7f9e1ce974c
petreastefann <petreastefann@gmail.com>
2023-12-06 00:03:14 +0200

    added the http package and logic to extract the request line from a request

--- Http/HttpParser.cpp
+++ Http/HttpParser.cpp
@@ -1,2 +1,18 @@
 #include "HttpParser.hpp"
 
+#include <Utils/Utils.hpp>
+
+HttpParser::HttpParser(const std::string& request) {
+}
+
+void HttpParser::extractRequestLineFromRequest(const std::string& request) {
+    std::vector<std::string> tokens = Utils::split(request, '\r\n');
+    std::vector<std::string> firstLineTokens = Utils::split(tokens[0], ' ');
+    std::vector<std::string> pathAndParams = Utils::split(firstLineTokens[1], '?');
+    std::vector<std::string> paramsAndValues = Utils::split(pathAndParams[1], '&');
+
+    m_requestType = firstLineTokens[0];
+    m_path = pathAndParams[0];
+    m_version = Utils::split(firstLineTokens[2], '/')[1];
+    m_queryParams = Utils::separateVectorOfKeysAndValues(paramsAndValues, '=');
+}
--- Http/HttpParser.hpp
+++ Http/HttpParser.hpp
@@ -1,13 +1,17 @@
 #pragma once
 #include <string>
 #include <unordered_map>
+#include <iostream>
 
 class HttpParser {
 public:
-			
+	HttpParser(const std::string& request);
+
 private:
 	std::string m_requestType;
 	std::string m_path;
 	std::string m_version;
     std::unordered_map<std::string, std::string> m_queryParams;
+
+	void extractRequestLineFromRequest(const std::string& request);
 };
