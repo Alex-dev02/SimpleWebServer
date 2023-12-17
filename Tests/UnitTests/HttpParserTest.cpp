a69d5c8eb197aa4a63788f20c73220118f8b0ab5
Alex-dev02 <51789497+Alex-dev02@users.noreply.github.com>
2023-12-17 15:39:18 +0200

    Revert "Http Parser"

--- Tests/UnitTests/HttpParserTest.cpp
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-#include <Http/HttpParser.hpp>
-
-TEST(HttpParserTest, ExtractHttpVerbFromRequest) {
-	HttpParser parser("GET /index.html?param1=value1&param2=value2 HTTP/1.1\r\n");
-
-	EXPECT_EQ("GET", parser.GetRequestType());
-}
